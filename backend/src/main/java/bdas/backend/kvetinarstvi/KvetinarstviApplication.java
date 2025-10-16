a89d293c40635339eadf45b57c6967edd8025188
frantisek-boura <frantisekboura2001@gmail.com>
2025-10-16 19:32:48 +0200

    zaklad backendu

---
+++ backend/src/main/java/bdas/backend/kvetinarstvi/KvetinarstviApplication.java
@@ -0,0 +1,13 @@
+package bdas.backend.kvetinarstvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvetinarstviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvetinarstviApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bdas/backend/kvetinarstvi/KvetinarstviApplicationTests.java
@@ -0,0 +1,13 @@
+package bdas.backend.kvetinarstvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvetinarstviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
