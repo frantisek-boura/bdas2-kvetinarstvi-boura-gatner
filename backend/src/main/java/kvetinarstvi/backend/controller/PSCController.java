618c98e85a12b93234fc31e9c95fe5ad3fa79d77
frantisek-boura <frantisekboura2001@gmail.com>
2025-11-06 12:50:04 +0100

    pridany controller a service pro psc

---
+++ backend/src/main/java/kvetinarstvi/backend/controller/PSCController.java
@@ -0,0 +1,54 @@
+package kvetinarstvi.backend.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kvetinarstvi.backend.records.PSC;
+import kvetinarstvi.backend.service.PSCService;
+
+@RestController
+@RequestMapping("/api/psc")
+public class PSCController {
+    
+    @Autowired
+    private PSCService service;
+
+    @GetMapping("")
+    public ResponseEntity<List<PSC>> getPSC() {
+        try {
+            List<PSC> psc = service.findAllPSC();
+
+            if (psc.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok(psc);
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().build();
+        } 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PSC> getPSCById(@PathVariable Integer id) {
+        try {
+            Optional<PSC> psc = service.findPSCById(id);
+
+            if (psc.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.ok(psc.get());
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().build();
+        } 
+    }
+
+}
--- backend/src/main/java/kvetinarstvi/backend/service/PSCService.java
+++ backend/src/main/java/kvetinarstvi/backend/service/PSCService.java
@@ -1,5 +1,11 @@
 package kvetinarstvi.backend.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.sql.DataSource;
@@ -15,8 +21,41 @@ public class PSCService {
     @Autowired
     private DataSource dataSource;
 
-    public Optional<PSC> findPSCById(Integer id) {
-        return null;
+    public List<PSC> findAllPSC() throws SQLException {
+        final String QUERY = "SELECT id_psc, psc FROM psc";
+        List<PSC> mesta = new ArrayList<>();
+
+        Connection c = dataSource.getConnection();
+        PreparedStatement stmt = c.prepareStatement(QUERY);        
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            int id_psc = rs.getInt("id_psc");
+            String psc = rs.getString("psc");
+
+            mesta.add(new PSC(id_psc, psc));
+        } 
+
+        return mesta;
+    }
+
+    public Optional<PSC> findPSCById(Integer id) throws SQLException {
+        final String QUERY = "SELECT id_psc, psc FROM psc WHERE id_psc = ?";
+
+        Connection c = dataSource.getConnection();
+        PreparedStatement stmt = c.prepareStatement(QUERY);        
+        stmt.setInt(1, id);
+
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            int id_psc = rs.getInt("id_psc");
+            String psc = rs.getString("psc");
+
+            return Optional.of(new PSC(id_psc, psc));
+        } else {
+            return Optional.empty();
+        }
     }
 
 }
