22fb3b1e943e67fcc53fc6865c36a0b0c92721f6
frantisek-boura <frantisekboura2001@gmail.com>
2025-11-06 18:03:39 +0100

    pridany controller a service pro obrazky

---
+++ backend/src/main/java/kvetinarstvi/backend/controller/ObrazekController.java
@@ -0,0 +1,54 @@
+package kvetinarstvi.backend.controller;
+
+import kvetinarstvi.backend.records.Mesto;
+import kvetinarstvi.backend.records.Obrazek;
+import kvetinarstvi.backend.service.ObrazekService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/obrazky")
+public class ObrazekController {
+
+    @Autowired
+    private ObrazekService service;
+
+    @GetMapping("")
+    public ResponseEntity<List<Obrazek>> getObrazky() {
+        try {
+            List<Obrazek> mesta = service.findAllObrazky();
+
+            if (mesta.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.ok(mesta);
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Obrazek> getObrazekById(@PathVariable Integer id) {
+        try {
+            Optional<Obrazek> obrazek = service.findObrazekById(id);
+
+            if (obrazek.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok(obrazek.get());
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+}
--- backend/src/main/java/kvetinarstvi/backend/service/ObrazekService.java
+++ backend/src/main/java/kvetinarstvi/backend/service/ObrazekService.java
@@ -1,9 +1,16 @@
 package kvetinarstvi.backend.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.sql.DataSource;
 
+import kvetinarstvi.backend.records.Obrazek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +22,42 @@ public class ObrazekService {
     @Autowired
     private DataSource dataSource;
 
-    public Optional<Obrazek> findObrazekById(Integer id) {
-        return null;
+    public List<Obrazek> findAllObrazky() throws SQLException {
+        final String QUERY = "SELECT id_obrazek, nazev_souboru, data FROM obrazky";
+        List<Obrazek> obrazky = new ArrayList<>();
+
+        Connection c = dataSource.getConnection();
+        PreparedStatement stmt = c.prepareStatement(QUERY);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            int id_obrazek = rs.getInt("id_obrazek");
+            String nazev_souboru = rs.getString("nazev_souboru");
+            byte[] data = rs.getBytes("data");
+
+            obrazky.add(new Obrazek(id_obrazek, nazev_souboru, data));
+        }
+
+        return obrazky;
+    }
+
+    public Optional<Obrazek> findObrazekById(Integer id) throws SQLException {
+        final String QUERY = "SELECT id_obrazek, nazev_souboru, data FROM obrazky WHERE id_obrazek = ?";
+
+        Connection c = dataSource.getConnection();
+        PreparedStatement stmt = c.prepareStatement(QUERY);
+        stmt.setInt(1, id);
+
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            int id_obrazek = rs.getInt("id_obrazek");
+            String nazev_souboru = rs.getString("nazev_souboru");
+            byte[] data = rs.getBytes("data");
+
+            return Optional.of(new Obrazek(id_obrazek, nazev_souboru, data));
+        } else {
+            return Optional.empty();
+        }
     }
 }
